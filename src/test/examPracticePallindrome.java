3b1b8441e00ac333e9df775582ebc6eaf1c3f98f
Miguel Martinez <miguelitalia@hotmail.com>
2019-03-31 11:03:24 +0100

    Java - added examPractice pallindrome code

--- src/test/examPracticePallindrome.java
+++ src/test/examPracticePallindrome.java
@@ -1,4 +1,38 @@
 package test;
 
+import java.util.Scanner;
+
 public class examPracticePallindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type in a string to check for pallindrome: ");
+        String word = input.nextLine();
+
+
+
+        if (isPalindrome(word)) {
+            System.out.println("It IS a pallindrome!");
+        } else {
+            System.out.println("It is NOT a pallindrome!");
+        }
+    }
+
+
+    public static boolean isPalindrome(String word) {
+        boolean is = true;
+        if (word.length() == 1) {
+            return is;
+        } else if (word.length() < 1) {
+            return is;
+        } else {
+            if (word.charAt(0) == word.charAt(word.length()-1)) {
+                isPalindrome(word.substring(1,word.length()-1));
+            } else {
+                is = false;
+            }
+        }
+        return is;
+    }
+
+
 }

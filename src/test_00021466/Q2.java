e433096443f436df6e055e2162375f6e8ab1d9e4
Miguel Martinez <miguelitalia@hotmail.com>
2019-04-06 11:51:11 +0100

    added java exam question 2 additional answer with only one loop but two variables to get suffix

--- src/test_00021466/Q2.java
+++ src/test_00021466/Q2.java
@@ -14,32 +14,39 @@ public class Q2 {
 	// write this method
 	public static String getLCS(String s1, String s2) {
 
-        String reS1 = "";
-        String reS2 = "";
-
-        for (int i = s1.length() - 1; i >= 0; i--) {
-            reS1 += s1.charAt(i);
-        }
-
-        for (int i = s2.length() - 1; i >= 0; i--) {
-            reS2 += s2.charAt(i);
-        }
-
-        int min = Math.min(reS1.length(), reS2.length());
-        String rev = "";
-
-        for (int i = 0; i < min; i++) {
-            if (reS1.charAt(i) == reS2.charAt(i)) {
-                rev += reS1.charAt(i);
-            }
-        }
+//        String reS1 = "";
+//        String reS2 = "";
+//
+//        for (int i = s1.length() - 1; i >= 0; i--) {
+//            reS1 += s1.charAt(i);
+//        }
+//
+//        for (int i = s2.length() - 1; i >= 0; i--) {
+//            reS2 += s2.charAt(i);
+//        }
+//
+//        int min = Math.min(reS1.length(), reS2.length());
+//        String rev = "";
+//
+//        for (int i = 0; i < min; i++) {
+//            if (reS1.charAt(i) == reS2.charAt(i)) {
+//                rev += reS1.charAt(i);
+//            }
+//        }
+//
+//        String end = "";
+//
+//        for (int i = rev.length() -1; i >= 0; i--) {
+//            end += rev.charAt(i);
+//        }
 
         String end = "";
 
-        for (int i = rev.length() -1; i >= 0; i--) {
-            end += rev.charAt(i);
+        for (int i = s1.length()-1, j = s2.length()-1;
+             i >= 0 && j >= 0 && s1.charAt(i) == s2.charAt(j);
+             i--, j--) {
+            end = s1.charAt(i) + end;
         }
-
 
         return end;
     }
